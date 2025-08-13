358a52e60c2b413d0ba13e5aead90a7feede29c2
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-08-13 18:27:47 +0530

    File upload service handling

---
+++ fileHandling-service/src/main/java/in/iiuzaifa/fileHandling_service/FileHandlingServiceApplication.java
@@ -0,0 +1,14 @@
+package in.iiuzaifa.fileHandling_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHandlingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileHandlingServiceApplication.class, args);
+		
+	}
+
+}
---
+++ fileHandling-service/src/test/java/in/iiuzaifa/fileHandling_service/FileHandlingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.iiuzaifa.fileHandling_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHandlingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
